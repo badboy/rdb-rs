fd49d1403c33074601810fad5fad44bf3770ed43
Jan-Erik Rediger <badboy@archlinux.us>
2014-12-30 20:41:38 +0100

    Basic Parsing already works.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+extern crate lzf;
+use std::str;
+use lzf::decompress;
+
 mod version {
-    pub const SUPPORTED_MINIMUM : u32 = 0;
+    pub const SUPPORTED_MINIMUM : u32 = 1;
     pub const SUPPORTED_MAXIMUM : u32 = 6;
 }
 
@@ -13,23 +17,23 @@ mod constants {
 }
 
 mod op_codes {
-    const EXPIRETIME_MS : u8 = 252;
-    const EXPIRETIME : u8 = 253;
-    const SELECTDB   : u8 = 254;
-    const EOF : u8 = 255;
+    pub const EXPIRETIME_MS : u8 = 252;
+    pub const EXPIRETIME : u8 = 253;
+    pub const SELECTDB   : u8 = 254;
+    pub const EOF : u8 = 255;
 }
 
 mod types {
-    const STRING : u8 = 0;
-    const LIST : u8 = 1;
-    const SET : u8 = 2;
-    const ZSET : u8 = 3;
-    const HASH : u8 = 4;
-    const HASH_ZIPMAP : u8 = 9;
-    const LIST_ZIPLIST : u8 = 10;
-    const SET_INTSET : u8 = 11;
-    const ZSET_ZIPLIST : u8 = 12;
-    const HASH_ZIPLIST : u8 = 13;
+    pub const STRING : u8 = 0;
+    pub const LIST : u8 = 1;
+    pub const SET : u8 = 2;
+    pub const ZSET : u8 = 3;
+    pub const HASH : u8 = 4;
+    pub const HASH_ZIPMAP : u8 = 9;
+    pub const LIST_ZIPLIST : u8 = 10;
+    pub const SET_INTSET : u8 = 11;
+    pub const ZSET_ZIPLIST : u8 = 12;
+    pub const HASH_ZIPLIST : u8 = 13;
 }
 
 mod encoding {
@@ -39,24 +43,129 @@ mod encoding {
     pub const LZF : u32 = 3;
 }
 
-#[deriving(Copy)]
+#[deriving(Show)]
 pub enum DataType {
-    String,
-    List,
-    Set,
-    SortedSet,
-    Hash,
+    String(Vec<u8>),
+    List(Vec<Vec<u8>>),
+    Set(Vec<Vec<u8>>),
+    SortedSet(Vec<(f32,Vec<u8>)>),
+    Hash(Vec<Vec<u8>>),
     Unknown
 }
 
-pub fn type_mapping(in_type: u8) -> DataType {
-    match in_type {
-        0 => DataType::String,
-        1 | 10 => DataType::List,
-        2 | 11 => DataType::Set,
-        3 | 12 => DataType::SortedSet,
-        4 | 9 | 13 => DataType::Hash,
-        _ => DataType::Unknown
+//pub fn type_mapping(in_type: u8) -> DataType {
+    //match in_type {
+        //0 => DataType::String,
+        //1 | 10 => DataType::List,
+        //2 | 11 => DataType::Set,
+        //3 | 12 => DataType::SortedSet,
+        //4 | 9 | 13 => DataType::Hash,
+        //_ => DataType::Unknown
+    //}
+//}
+
+pub fn parse<T: Reader>(input: &mut T) {
+
+    assert!(verify_magic(input));
+    assert!(verify_version(input));
+
+    loop {
+        let next_op = input.read_byte().unwrap();
+        //println!("next_op: {:x}", next_op);
+
+        match next_op {
+            op_codes::SELECTDB => {
+                let db = input.read_byte().unwrap();
+                println!("SELECTDB: {}", db);
+            },
+            op_codes::EOF => {
+                println!("EOF");
+                break ;
+            },
+            op_codes::EXPIRETIME_MS => {
+                let expiretime_ms = input.read_le_u64().unwrap();
+                println!("EXPIRETIME_MS: {}", expiretime_ms);
+            },
+            op_codes::EXPIRETIME => {
+                let expiretime = input.read_be_u32().unwrap();
+                println!("EXPIRETIME: {}", expiretime);
+            }
+            _ => {
+                let key = read_blob(input);
+                println!("Key: {}",
+                         str::from_utf8(key.as_slice()).unwrap());
+                println!("Val: {}", read_type(next_op, input));
+                break;
+            }
+        }
+
+    }
+
+    return;
+
+}
+
+fn read_type<T: Reader>(value_type: u8, input: &mut T) -> DataType {
+    match value_type {
+        types::STRING => {
+            DataType::String(read_blob(input))
+        },
+        types::LIST => {
+            //DataType::List(read_linked_list(input))
+
+            let mut len = read_length(input);
+            let mut list = vec![];
+
+            while len > 0 {
+                let blob = read_blob(input);
+                list.push(blob);
+                len -= 1;
+            }
+
+            DataType::List(list)
+        },
+        types::SET => {
+            println!("Value Type not implemented: {}", "SET");
+
+            //DataType::Set(read_set(input))
+            DataType::Set(vec![])
+        },
+        types::ZSET => {
+            println!("Value Type not implemented: {}", "ZSET");
+            //DataType::SortedSet(read_sorted_set(input))
+            DataType::SortedSet(vec![])
+        },
+        types::HASH => {
+            println!("Value Type not implemented: {}", "HASH");
+            //DataType::Hash(read_hash(input))
+            DataType::Hash(vec![])
+        },
+        types::HASH_ZIPMAP => {
+            println!("Value Type not implemented: {}", "HASH_ZIPMAP");
+            //DataType::Hash(read_hash_zipmap(input))
+            DataType::Hash(vec![])
+        },
+        types::LIST_ZIPLIST => {
+            println!("Value Type not implemented: {}", "LIST_ZIPLIST");
+            //DataType::List(read_list_ziplist(input))
+            DataType::List(vec![])
+        },
+        types::SET_INTSET => {
+            println!("Value Type not implemented: {}", "SET_INTSET");
+            //DataType::Set(read_set_intset(input))
+            DataType::Set(vec![])
+        },
+        types::ZSET_ZIPLIST => {
+            println!("Value Type not implemented: {}", "ZSET_ZIPLIST");
+            //DataType::SortedSet(read_zset_ziplist(input))
+            DataType::SortedSet(vec![])
+        },
+        types::HASH_ZIPLIST => {
+            println!("Value Type not implemented: {}", "HASH_ZIPLIST");
+            //DataType::Hash(read_hash_ziplist(input))
+            DataType::Hash(vec![])
+        },
+        _ => { panic!("Value Type not implemented: {}", value_type) }
     }
 }
 
@@ -79,8 +188,8 @@ fn verify_version<T: Reader>(input: &mut T) -> bool {
         (version[2]-48) as u32 * 10 +
         (version[3]-48) as u32;
 
-    version > version::SUPPORTED_MINIMUM &&
-        version < version::SUPPORTED_MAXIMUM
+    version >= version::SUPPORTED_MINIMUM &&
+        version <= version::SUPPORTED_MAXIMUM
 }
 
 #[test]
@@ -147,23 +256,30 @@ fn read_length_with_encoding<T: Reader>(input: &mut T) -> LengthEncoded {
     LengthEncoded::LE(length, is_encoded)
 }
 
+fn read_length<T: Reader>(input: &mut T) -> u32 {
+    let LengthEncoded::LE(length, _) = read_length_with_encoding(input);
+    length
+}
+
 fn read_blob<T: Reader>(input: &mut T) -> Vec<u8> {
     let LengthEncoded::LE(length, is_encoded) = read_length_with_encoding(input);
 
-    let mut val : Vec<u8>;
     if is_encoded {
-        val = match length {
+        match length {
             encoding::INT8 => { input.read_exact(1).unwrap() },
             encoding::INT16 => { input.read_exact(2).unwrap() },
             encoding::INT32 => { input.read_exact(4).unwrap() },
-            encoding::LZF => { panic!("Encoding not implemented. LZF") },
-            _ => { panic!("Unknown encoding: {}", is_encoded) }
+            encoding::LZF => {
+                let compressed_length = read_length(input);
+                let real_length = read_length(input);
+                let data = input.read_exact(compressed_length as uint).unwrap();
+                lzf::decompress(data.as_slice(), real_length as uint).unwrap()
+            },
+            _ => { panic!("Unknown encoding: {}", length) }
         }
     } else {
-        val = input.read_exact(length as uint).unwrap();
+        input.read_exact(length as uint).unwrap()
     }
-
-    val
 }
 
 #[test]
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![feature(globs)]
+
+extern crate rdb;
+use std::os;
+use std::io::{BufferedReader, File};
+
+use rdb::*;
+
+fn main() {
+    let args = os::args();
+    if args.len() == 1 {
+        println!("Usage: {} [list of files]", args[0]);
+        panic!();
+    }
+
+    let file = File::open(&Path::new(args[1].to_string()));
+    let mut reader = BufferedReader::new(file);
+
+    rdb::parse(&mut reader);
+}
