02dab094e2be3428ab3702b609bcd218b89783c7
Jan-Erik Rediger <janerik@fnordig.de>
2015-01-07 23:01:05 +0100

    Make it public to avoid warnings

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate rdb;
 use std::os;
 use std::io::{BufferedReader, File};
 
-fn main() {
+pub fn main() {
     let args = os::args();
     if args.len() == 1 {
         println!("Usage: {} [list of files]", args[0]);
