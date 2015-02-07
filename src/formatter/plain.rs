26c2ae7a4fc01ea2083a387ed5ff338936a44b90
Jan-Erik Rediger <janerik@fnordig.de>
2015-02-07 17:16:03 +0100

    Finish plain formatter to match old `diff` output

--- src/formatter/plain.rs
+++ src/formatter/plain.rs
@@ -1,55 +1,111 @@
+#![allow(unused_must_use)]
 use formatter::Formatter;
 use std::old_io;
+use serialize::hex::ToHex;
+use types::EncodingType;
 
 pub struct Plain {
-    out: Box<Writer+'static>
+    out: Box<Writer+'static>,
+    dbnum: u32,
+    index: u32
 }
 
 impl Plain {
     pub fn new() -> Plain {
         let out = Box::new(old_io::stdout());
-        Plain { out: out }
+        Plain { out: out, dbnum: 0, index: 0 }
+    }
+
+    fn write_line_start(&mut self) {
+        self.out.write_str(format!("db={} ", self.dbnum).as_slice());
     }
 }
 
 impl Formatter for Plain {
-    fn start_rdb(&mut self) {
-        println!("Start of RDB");
-    }
-
-    fn end_rdb(&mut self) {
-        println!("End of RDB");
-    }
-
     fn checksum(&mut self, checksum: &[u8]) {
-        let _ = self.out.write_str("Checksum: ");
-        let _ = self.out.write(checksum.as_slice());
-        let _ = self.out.write_str("\n");
+        self.out.write_str("checksum ");
+        self.out.write_str(checksum.to_hex().as_slice());
+        self.out.write_str("\n");
     }
 
     fn start_database(&mut self, db_number: u32) {
-        println!("SELECTDB: {}", db_number);
-    }
-
-    fn end_database(&mut self, db_number: u32) {
-        println!("END_DB: {}", db_number);
+        self.dbnum = db_number;
     }
 
     fn set(&mut self, key: &[u8], value: &[u8], _expiry: Option<u64>) {
-        let _ = self.out.write(key.as_slice());
-        let _ = self.out.write_str(": ");
+        self.write_line_start();
+        self.out.write(key.as_slice());
+        self.out.write_str(" -> ");
 
-        let _ = self.out.write(value.as_slice());
-        let _ = self.out.write_str("\n");
-        let _ = self.out.flush();
+        self.out.write(value.as_slice());
+        self.out.write_str("\n");
+        self.out.flush();
     }
 
     fn aux_field(&mut self, key: &[u8], value: &[u8]) {
-        let _ = self.out.write_str("[aux] ");
-        let _ = self.out.write(key.as_slice());
-        let _ = self.out.write_str(": ");
-        let _ = self.out.write(value.as_slice());
-        let _ = self.out.write_str("\n");
-        let _ = self.out.flush();
+        self.out.write_str("aux ");
+        self.out.write(key.as_slice());
+        self.out.write_str(" -> ");
+        self.out.write(value.as_slice());
+        self.out.write_str("\n");
+        self.out.flush();
+    }
+
+    fn hash_element(&mut self, key: &[u8], field: &[u8], value: &[u8]) {
+        self.write_line_start();
+
+        self.out.write(key.as_slice());
+        self.out.write_str(" . ");
+        self.out.write(field.as_slice());
+        self.out.write_str(" -> ");
+        self.out.write(value.as_slice());
+        self.out.write_str("\n");
+        self.out.flush();
+    }
+
+    fn set_element(&mut self, key: &[u8], member: &[u8]) {
+        self.write_line_start();
+
+        self.out.write(key.as_slice());
+        self.out.write_str(" { ");
+        self.out.write(member.as_slice());
+        self.out.write_str(" } ");
+        self.out.write_str("\n");
+        self.out.flush();
+    }
+
+    fn start_list(&mut self, _key: &[u8], _length: u32,
+                  _expiry: Option<u64>, _info: EncodingType) {
+        self.index = 0;
+    }
+    fn list_element(&mut self, key: &[u8], value: &[u8]) {
+        self.write_line_start();
+
+        self.out.write(key.as_slice());
+        self.out.write_str(format!("[{}]", self.index).as_slice());
+        self.out.write_str(" -> ");
+        self.out.write(value.as_slice());
+        self.out.write_str("\n");
+        self.out.flush();
+        self.index += 1;
+    }
+
+    fn start_sorted_set(&mut self, _key: &[u8], _length: u32,
+                        _expiry: Option<u64>, _info: EncodingType) {
+        self.index = 0;
+    }
+
+    fn sorted_set_element(&mut self, key: &[u8],
+                          score: f64, member: &[u8]) {
+        self.write_line_start();
+
+        self.out.write(key.as_slice());
+        self.out.write_str(format!("[{}]", self.index).as_slice());
+        self.out.write_str(" -> {");
+        self.out.write(member.as_slice());
+        self.out.write_str(format!(", score={}", score).as_slice());
+        self.out.write_str("}\n");
+        self.out.flush();
+        self.index += 1;
     }
 }
