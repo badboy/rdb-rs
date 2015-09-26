2d1055f4f257bbda1e1a7c44dae3aeb1495ed8aa
Jan-Erik Rediger <janerik@fnordig.de>
2015-09-26 18:33:53 +0200

    style: Move CLI tool to subdirectory
    Cargo automatically picks it up from there. No need for special
    configuration anymore.

---
+++ src/bin/rdb.rs
@@ -0,0 +1,22 @@
+extern crate rdb;
+extern crate getopts;
+extern crate regex;
+use std::io::BufReader;
+use std::fs::File;
+use std::path::Path;
+
+fn main() {
+    let path = "dump.rdb";
+    let file = File::open(&Path::new(&*path)).unwrap();
+    let reader = BufReader::new(file);
+    let filter = rdb::filter::Simple::new();
+
+    let mut parser = rdb::parse(reader, rdb::formatter::Nil::new(), filter);
+
+
+    parser.parse().unwrap();
+
+    while let Ok(val) = parser.advance() {
+        println!("{:?}", val);
+    }
+}
