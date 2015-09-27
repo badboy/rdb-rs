6652764f78203d0fb6518dcb7d08f374953d7064
Jan-Erik Rediger <janerik@fnordig.de>
2015-09-27 03:23:07 +0200

    perf: Replace read_exact with stdlib code

--- src/helper.rs
+++ src/helper.rs
@@ -1,44 +1,42 @@
 use std::io::Read;
 use std::io::Result as IoResult;
+use std::io::ErrorKind;
 
 pub fn int_to_vec(number: i32) -> Vec<u8> {
-  let number = number.to_string();
-  let mut result = Vec::with_capacity(number.len());
-  for &c in number.as_bytes().iter() {
-    result.push(c);
-  }
-  result
-}
-
-fn try_read<T: Read>(reader: &mut T, buf : &mut [u8], min_bytes : usize) -> IoResult<usize> {
-    let mut pos = 0;
-    let buf_len = buf.len();
-    while pos < min_bytes {
-        let buf1 = &mut buf[pos .. buf_len];
-        let n = try!(reader.read(buf1));
-        pos += n;
-        if n == 0 { return Ok(pos);  }
-
+    let number = number.to_string();
+    let mut result = Vec::with_capacity(number.len());
+    for &c in number.as_bytes().iter() {
+        result.push(c);
     }
-    return Ok(pos);
-
+    result
 }
 
-fn read<T: Read>(reader: &mut T, buf : &mut [u8], min_bytes : usize) -> IoResult<usize> {
-    let n = try!(try_read(reader, buf, min_bytes));
-    if n < min_bytes {
-        Err(::std::io::Error::new(::std::io::ErrorKind::Other,
-                                  "Could not read enough bytes from Reader"))
-    } else {
-        Ok(n)
-    }
-}
-
+// Taken from Rust:
+// https://github.com/rust-lang/rust/blob/6f880eee792e974b18cbb129fd16928939589e7c/src/libstd/io/mod.rs#L596-L611
 pub fn read_exact<T: Read>(reader: &mut T, len: usize) -> IoResult<Vec<u8>> {
-    let mut buf = Vec::with_capacity(len);
-    unsafe { buf.set_len(len); }
+    let mut out = Vec::with_capacity(len);
+    unsafe {
+        out.set_len(len);
+    }
 
-    try!(read(reader, &mut buf, len));
+    {
+        let mut buf = &mut *out;
+        while !buf.is_empty() {
+            match reader.read(buf) {
+                Ok(0) => break,
+                Ok(n) => {
+                    let tmp = buf;
+                    buf = &mut tmp[n..];
+                }
+                Err(ref e) if e.kind() == ErrorKind::Interrupted => {}
+                Err(e) => return Err(e),
+            }
+        }
+        if !buf.is_empty() {
+            return Err(Error::new(ErrorKind::Interrupted,
+                                  "failed to fill whole buffer"))
+        }
+    }
 
-    Ok(buf)
+    Ok(out)
 }
